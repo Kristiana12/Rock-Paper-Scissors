104df8ca53264a674eed2268fc049fa035015440
Kristiana Leka <63458911+Kristiana12@users.noreply.github.com>
2021-11-16 07:56:13 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -9,34 +9,29 @@ const arrowImg = document.querySelector("#middle-text img");
 const reset = document.querySelector(".reset");
 
 const playerCards = document.querySelectorAll(".player-card");
-const playerCardOnDeck1 = document.querySelector(".player-card-1 img");
-const playerCardOnDeck2 = document.querySelector(".player-card-2 img");
-const playerCardOnDeck3 = document.querySelector(".player-card-3 img");
-const playerCardOnDeck4 = document.querySelector(".player-card-4 img");
+const playerCardImg = document.querySelectorAll(".player-card img");
 
 const opponentCards = document.querySelectorAll(".opponent-card");
 const opponentCardIcons = document.querySelectorAll(".opponent-card img");
 
-const randomNumberPlayer1 = Math.floor(Math.random() * 3);
-const randomNumberPlayer2 = Math.floor(Math.random() * 3);
-const randomNumberPlayer3 = Math.floor(Math.random() * 3);
-const randomNumberComputer1 = Math.floor(Math.random() * 3);
-const randomNumberComputer2 = Math.floor(Math.random() * 3);
-const randomNumberComputer3 = Math.floor(Math.random() * 3);
-
 let isPlaying = true;
 let numberOfCardsLeft = 3;
+let randomNumber = [];
 
-//FIXME;
-// const cardsLeftToPlay = () => {
-//   numberOfCardsLeft--;
-//   if (numberOfCardsLeft === 0) isPlaying = false;
+const generateRandomNumber = () => {
+  return Math.floor(Math.random() * 3);
+};
 
-//   if (!isPlaying) {
-//     reset.style.display = "block";
-//     middleText.style.display = "none";
-//   }
-// };
+//Checks if there are still cards to play
+const cardsLeftToPlay = () => {
+  numberOfCardsLeft--;
+  if (numberOfCardsLeft === 0) isPlaying = false;
+
+  if (!isPlaying) {
+    reset.style.display = "block";
+    middleText.style.display = "none";
+  }
+};
 
 //Show Instructions
 instructions.onclick = function () {
@@ -48,45 +43,42 @@ instructionsList.onclick = function () {
 };
 
 //Get Player Icons
-const getPlayerRandomIcon = () => {
-  playerCardOnDeck2.setAttribute(
-    "src",
-    `images/icon-${randomNumberPlayer1}.svg`
-  );
-  playerCardOnDeck3.setAttribute(
-    "src",
-    `images/icon-${randomNumberPlayer2}.svg`
-  );
-  playerCardOnDeck4.setAttribute(
-    "src",
-    `images/icon-${randomNumberPlayer3}.svg`
-  );
+const getRandomIcons = () => {
+  for (let counter = 0; counter < 6; counter++) {
+    randomNumber.push(generateRandomNumber());
+  }
+
+  playerCardImg[1].setAttribute("src", `images/icon-${randomNumber[0]}.svg`);
+  playerCardImg[2].setAttribute("src", `images/icon-${randomNumber[1]}.svg`);
+  playerCardImg[3].setAttribute("src", `images/icon-${randomNumber[2]}.svg`);
 };
 
-//FIXME;
 //Reset Button
-// const resetGame = () => {
-//   reset.style.display = "none";
-//   middleText.style.display = "block";
-//   middleText.textContent = "Pick a card to play!";
-//   middleText.style.color = "#ffcd1e";
+const resetGame = () => {
+  reset.style.display = "none";
+  middleText.style.display = "block";
+  middleText.textContent = "Pick a card to play!";
+  middleText.style.color = "#ffcd1e";
 
-//   // Show Player cards
-//   for (let i = 1; i < playerCards.length; i++) {
-//     playerCards[i].style.visibility = "visible";
-//   }
-//   playerCards[0].style.visibility = "hidden";
-//   playerCardOnDeck1.style.display = "none";
+  // Show Player cards
+  for (let i = 1; i < playerCards.length; i++) {
+    playerCards[i].style.visibility = "visible";
+  }
+  playerCards[0].style.visibility = "hidden";
+  playerCardImg[0].style.visibility = "hidden";
 
-//   //Show Opponent cards
-//   for (let i = 0; i < 3; i++) {
-//     opponentCards[i].style.visibility = "visible";
-//   }
-//   opponentCards[3].style.visibility = "hidden";
-//   opponentCardIcons[3].style.display = "none";
+  //Show Opponent cards
+  for (let i = 0; i < 3; i++) {
+    opponentCards[i].style.visibility = "visible";
+  }
+  opponentCards[3].style.visibility = "hidden";
+  opponentCardIcons[3].style.visibility = "hidden";
 
-//   getPlayerRandomIcon();
-// };
+  isPlaying = true;
+  numberOfCardsLeft = 3;
+  randomNumber = [];
+  getRandomIcons();
+};
 
 const pickACard = (number1, number2) => {
   //Hide selected player card and one opponent card.
@@ -98,7 +90,7 @@ const pickACard = (number1, number2) => {
   opponentCardIcons[3].style.visibility = "visible";
   //Show the Selected Card you picked to play
   playerCards[0].style.visibility = "visible";
-  playerCardOnDeck1.style.visibility = "visible";
+  playerCardImg[0].style.visibility = "visible";
 
   arrowImg.style.visibility = "hidden";
 };
@@ -118,57 +110,48 @@ const checkWinner = (playerNum, computerNum) => {
     middleText.textContent = "Yay, you won!";
     middleText.style.color = "#ffcd1e";
   }
-  // setTimeout(cardsLeftToPlay, 2000);
+
+  //Show Reset Button 2s after the game ends
+  setTimeout(cardsLeftToPlay, 2000);
 };
 
 const pickACard2 = () => {
   //Chooses the icons on played cards
-  playerCardOnDeck1.setAttribute(
-    "src",
-    `images/icon-${randomNumberPlayer1}.svg`
-  );
-
+  playerCardImg[0].setAttribute("src", `images/icon-${randomNumber[0]}.svg`);
   opponentCardIcons[3].setAttribute(
     "src",
-    `images/icon-${randomNumberComputer1}.svg`
+    `images/icon-${randomNumber[3]}.svg`
   );
-  //Shows the cards that are played and hides.
+  //Hides the Selected card and chooses the icon on the selected card
   pickACard(1, 1);
-  checkWinner(randomNumberPlayer1, randomNumberComputer1);
+  checkWinner(randomNumber[0], randomNumber[3]);
 };
 
 //Hides the Selected card and chooses the icon on the selected card
 const pickACard3 = () => {
-  playerCardOnDeck1.setAttribute(
-    "src",
-    `images/icon-${randomNumberPlayer2}.svg`
-  );
-
-  opponentCards[3].style.visibility = "visible";
-  opponentCardIcons[3].style.visibility = "visible";
-
+  //Chooses the icons on played cards
+  playerCardImg[0].setAttribute("src", `images/icon-${randomNumber[1]}.svg`);
   opponentCardIcons[3].setAttribute(
     "src",
-    `images/icon-${randomNumberComputer2}.svg`
+    `images/icon-${randomNumber[4]}.svg`
   );
-
+  //Hides the Selected card and chooses the icon on the selected card
   pickACard(2, 2);
-  checkWinner(randomNumberPlayer2, randomNumberComputer2);
+  checkWinner(randomNumber[1], randomNumber[4]);
 };
 
 //Hides the Selected card and chooses the icon on the selected card
 const pickACard4 = () => {
-  playerCardOnDeck1.setAttribute(
-    "src",
-    `images/icon-${randomNumberPlayer3}.svg`
-  );
-
+  //Chooses the icons on played cards
+  playerCardImg[0].setAttribute("src", `images/icon-${randomNumber[2]}.svg`);
   opponentCardIcons[3].setAttribute(
     "src",
-    `images/icon-${randomNumberComputer3}.svg`
+    `images/icon-${randomNumber[5]}.svg`
   );
+
+  //Hides the Selected card and chooses the icon on the selected card
   pickACard(3, 0);
-  checkWinner(randomNumberPlayer3, randomNumberComputer3);
+  checkWinner(randomNumber[2], randomNumber[5]);
 };
 
 //Start Button
@@ -186,12 +169,11 @@ const startGame = () => {
   for (let i = 0; i < 3; i++) {
     opponentCardIcons[i].style.display = "none";
   }
-  getPlayerRandomIcon();
+  getRandomIcons();
 };
 
 startBtn.addEventListener("click", startGame);
 playerCards[1].addEventListener("click", pickACard2);
 playerCards[2].addEventListener("click", pickACard3);
 playerCards[3].addEventListener("click", pickACard4);
-TODO;
-//reset.addEventListener("click", resetGame);
+reset.addEventListener("click", resetGame);
